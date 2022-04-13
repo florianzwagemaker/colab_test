d0563ec74664c5bdb2aa163a0a70ae4aa34e5693
Florian Zwagemaker <f.zwagemaker@gmail.com>
2022-04-13 16:59:40 +0200

    add quiet flag to conda & mamba

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 wget -q https://repo.anaconda.com/miniconda/Miniconda3-py37_4.11.0-Linux-x86_64.sh
 chmod +x Miniconda3-py37_4.11.0-Linux-x86_64.sh
 ./Miniconda3-py37_4.11.0-Linux-x86_64.sh -b -f -p /usr/local > /dev/null
-conda install -c conda-forge mamba -y > /dev/null
-mamba install -c conda-forge -c bioconda -c intel -c anaconda minimap2 samtools seqkit fastp -y > /dev/null
+conda install -q -c conda-forge mamba -y > /dev/null
+mamba install -q -c conda-forge -c bioconda -c intel -c anaconda minimap2 samtools seqkit fastp -y > /dev/null
 
 echo "Setup is done, you can now continue with the course material"
