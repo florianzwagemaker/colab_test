fafa389b877c2d99bbcfe70cdc656b6c1a9533b8
Florian Zwagemaker <f.zwagemaker@gmail.com>
2022-04-13 16:30:23 +0200

    setup test 2

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
-MINICONDA_INSTALLER_SCRIPT=Miniconda3-py37_4.11.0-Linux-x86_64.sh
-MINICONDA_PREFIX=/usr/local
-wget https://repo.anaconda.com/miniconda/$MINICONDA_INSTALLER_SCRIPT
-chmod +x $MINICONDA_INSTALLER_SCRIPT
-./$MINICONDA_INSTALLER_SCRIPT -b -f -p $MINICONDA_PREFIX > /dev/null
+#!/bin/bash
+wget -q https://repo.anaconda.com/miniconda/Miniconda3-py37_4.11.0-Linux-x86_64.sh
+chmod +x Miniconda3-py37_4.11.0-Linux-x86_64.sh
+./Miniconda3-py37_4.11.0-Linux-x86_64.sh -b -f -p /usr/local > /dev/null
 conda install -c conda-forge mamba -y > /dev/null
 mamba install -c conda-forge -c bioconda -c intel -c anaconda minimap2 samtools seqkit fastp -y > /dev/null
 
