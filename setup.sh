243035a59275275722fee7b3f95cf85ba07a75b1
Florian Zwagemaker <f.zwagemaker@gmail.com>
2022-04-19 15:55:34 +0200

    fix reference fasta link

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ mkdir example_data
 bash -c "$(wget -q ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR118/078/SRR11844878/SRR11844878_1.fastq.gz -O example_data/sars-cov-2_hong_kong_example.fastq.gz && echo 'Done with downloading the example data')" & 
 
 ## fetch the reference genome from NCBI
-bash -c "$(wget -q 'https://www.ncbi.nlm.nih.gov/sviewer/viewer.cgi?tool=portal&save=file&log$=seqview&db=nuccore&report=fasta&id=1798172431&extrafeat=null&conwithfeat=on&hide-cdd=on' -O example_data/sars-cov-2_referebce.fasta && echo 'Done with downloading the reference genome')" &
+bash -c "$(wget -q https://raw.githubusercontent.com/RIVM-bioinformatics/SARS2seq/main/SARS2seq/workflow/files/MN908947.fasta -O example_data/sars-cov-2_referebce.fasta && echo 'Done with downloading the reference genome')" &
 
 
 ## install miniconda and get some basic tools
