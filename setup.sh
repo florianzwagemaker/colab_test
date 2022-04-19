e6f1a64c4177cd59647b393df49efb1bb2ca4245
Florian Zwagemaker <f.zwagemaker@gmail.com>
2022-04-19 15:56:12 +0200

    remove miniconda setup script when its done

--- setup.sh
+++ setup.sh
@@ -13,6 +13,7 @@ bash -c "$(wget -q https://raw.githubusercontent.com/RIVM-bioinformatics/SARS2se
 wget -q https://repo.anaconda.com/miniconda/Miniconda3-py37_4.11.0-Linux-x86_64.sh
 chmod +x Miniconda3-py37_4.11.0-Linux-x86_64.sh
 ./Miniconda3-py37_4.11.0-Linux-x86_64.sh -b -f -p /usr/local > /dev/null 2>&1
+rm Miniconda3-py37_4.11.0-Linux-x86_64.sh
 conda install -q -c conda-forge mamba -y > /dev/null 2>&1
 mamba install -q -c conda-forge -c bioconda -c intel -c anaconda minimap2 samtools seqkit fastp -y > /dev/null 2>&1
 
