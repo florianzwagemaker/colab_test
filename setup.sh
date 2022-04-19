bffff21da3be29e60244d192ab63f8fe0fdafb7e
Florian Zwagemaker <f.zwagemaker@gmail.com>
2022-04-19 15:53:47 +0200

    fix quote chars

--- setup.sh
+++ setup.sh
@@ -3,10 +3,10 @@
 ## fetch an example fastq file from ENA
 rm -rf /content/sample_data
 mkdir example_data
-bash -c "$(wget -q ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR118/078/SRR11844878/SRR11844878_1.fastq.gz -O example_data/sars-cov-2_hong_kong_example.fastq.gz && echo "Done with downloading the example data")" & 
+bash -c "$(wget -q ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR118/078/SRR11844878/SRR11844878_1.fastq.gz -O example_data/sars-cov-2_hong_kong_example.fastq.gz && echo 'Done with downloading the example data')" & 
 
 ## fetch the reference genome from NCBI
-bash -c "$(wget -q 'https://www.ncbi.nlm.nih.gov/sviewer/viewer.cgi?tool=portal&save=file&log$=seqview&db=nuccore&report=fasta&id=1798172431&extrafeat=null&conwithfeat=on&hide-cdd=on' -O example_data/sars-cov-2_referebce.fasta && echo "Done with downloading the reference genome")" &
+bash -c "$(wget -q 'https://www.ncbi.nlm.nih.gov/sviewer/viewer.cgi?tool=portal&save=file&log$=seqview&db=nuccore&report=fasta&id=1798172431&extrafeat=null&conwithfeat=on&hide-cdd=on' -O example_data/sars-cov-2_referebce.fasta && echo 'Done with downloading the reference genome')" &
 
 
 ## install miniconda and get some basic tools
