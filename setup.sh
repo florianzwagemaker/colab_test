cbe85be9ed6430c48669c7309a23b501bba98c95
Florian Zwagemaker <f.zwagemaker@gmail.com>
2022-04-19 15:50:04 +0200

    some changes to setup script + fastp command fix

--- setup.sh
+++ setup.sh
@@ -3,7 +3,11 @@
 ## fetch an example fastq file from ENA
 rm -rf /content/sample_data
 mkdir example_data
-wget -q ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR118/078/SRR11844878/SRR11844878_1.fastq.gz -O example_data/sars-cov-2_hong_kong_example.fastq.gz &
+bash -c "$(wget -q ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR118/078/SRR11844878/SRR11844878_1.fastq.gz -O example_data/sars-cov-2_hong_kong_example.fastq.gz && echo "Done with downloading the example data")" & 
+
+## fetch the reference genome from NCBI
+bash -c "$(wget -q https://www.ncbi.nlm.nih.gov/sviewer/viewer.cgi?tool=portal&save=file&log$=seqview&db=nuccore&report=fasta&id=1798172431&extrafeat=null&conwithfeat=on&hide-cdd=on -O example_data/sars-cov-2_referebce.fasta && echo "Done with downloading the reference genome")" &
+
 
 ## install miniconda and get some basic tools
 wget -q https://repo.anaconda.com/miniconda/Miniconda3-py37_4.11.0-Linux-x86_64.sh
@@ -12,4 +16,4 @@ chmod +x Miniconda3-py37_4.11.0-Linux-x86_64.sh
 conda install -q -c conda-forge mamba -y > /dev/null 2>&1
 mamba install -q -c conda-forge -c bioconda -c intel -c anaconda minimap2 samtools seqkit fastp -y > /dev/null 2>&1
 
-echo "Setup is done, you can now continue with the course material"
+echo "Installation of Miniconda and some basic tools is done"
